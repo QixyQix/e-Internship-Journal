245e954ce47532b2e3117f3da57c4548a9513aac
Unknown <tanchiawei.15@ichat.sp.edu.sg>
2017-10-26 15:09:31 +0800

    New Batch User Models
    New Batch User Models

---
+++ E-Internship Journal/Models/UserBatchModels/Batch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Internship_Journal.Models.UserBatchModels
+{
+    public class Batch
+    {
+        public int BatchId { get; set; }
+        public string BatchName { get; set; }
+
+    }
+}
