5707b6392c6a124a76cba723cd6af5facad65fda
Unknown <tanchiawei.15@ichat.sp.edu.sg>
2017-10-26 14:47:14 +0800

    Initial project files
    Initial Project files

---
+++ E-Internship Journal/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using E_Internship_Journal.Models;
+
+namespace E_Internship_Journal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Frequently used command in Command Prompt
+            //To generate migration files:
+            //>dnu restore
+            //>dnx ef migrations add setup_db_file
+            //Reference: http://www.bricelam.net/2014/09/14/migrations-on-k.html
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=E_Internship_Journal;Trusted_Connection=True;MultipleActiveResultSets=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
