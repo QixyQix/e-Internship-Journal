b02cd7caef576b0ae6647399b14bbb68c0d03051
Chia Wei <tanchiawei.15@ichat.sp.edu.sg>
2017-11-03 00:12:08 +0800

    Changes to APi Controller
    Add proper return object values for API methods.
    Fix SupervisorId where by it requires to execute await first then get the .id
    
    Modified UserBatches to accept Get/{id} and Post method

--- E-Internship Journal/API/ProjectsController.cs
+++ E-Internship Journal/API/ProjectsController.cs
@@ -21,39 +21,94 @@ namespace E_Internship_Journal.API
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        public ProjectsController(ApplicationDbContext context , UserManager<ApplicationUser> userManager,
+        public ProjectsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
         }
-        
+
         // GET: api/Projects
         [HttpGet]
         [AllowAnonymous]
-        public IEnumerable<Project> GetProjectsAsync()
+        public IActionResult GetProjects()
         {
-            return _context.Projects;
+
+            //var ww = rr.Id;
+            //var ttt = _userManager.GetUserId(rr);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            List<object> projectList = new List<object>();
+            var projects = _context.Projects
+            .Include(eacbProjectEntity => eacbProjectEntity.Supervisor)
+            .Include(eacbProjectEntity => eacbProjectEntity.Company)
+            .AsNoTracking();
+            foreach (var oneProject in projects)
+            {
+                //   List<int> categoryIdList = new List<int>();
+                projectList.Add(new
+                {
+                    oneProject.ProjectId,
+                    oneProject.ProjectName,
+                    oneProject.Supervisor.Email,
+                    oneProject.Company.CompanyName,
+                    oneProject.Company.CompanyAddress
+                });
+            }
+
+            return new JsonResult(projectList);
         }
 
         // GET: api/Projects/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetProject([FromRoute] int id)
+        public IActionResult GetProject([FromRoute] int id)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var project = await _context.Projects.SingleOrDefaultAsync(m => m.ProjectId == id);
-
-            if (project == null)
+            if (ProjectExists(id))
             {
-                return NotFound();
+                try
+                {
+                    var foundProject = _context.Projects
+                    .Where(eacbProjectEntity => eacbProjectEntity.ProjectId == id)
+                    .Include(eacbProjectEntity => eacbProjectEntity.Supervisor)
+                    .Include(eacbProjectEntity => eacbProjectEntity.Company).Single();
+                    var response = new
+                    {
+                        ProjectId = foundProject.ProjectId,
+                        ProjectName = foundProject.ProjectName,
+                        CompanyName = foundProject.Company.CompanyName,
+                        CompanyAddress = foundProject.Company.CompanyAddress,
+                        FullName = foundProject.Supervisor.FullName
+                    };//end of creation of the response object
+                    return new JsonResult(response);
+                }
+                catch (Exception exceptionObject)
+                {
+                    //Create a fail message anonymous object
+                    //This anonymous object only has one Message property 
+                    //which contains a simple string message
+                    object httpFailRequestResultMessage =
+                    new { Message = "Unable to obtain brand information." };
+                    //Return a bad http response message to the client
+                    return BadRequest(httpFailRequestResultMessage);
+                }
             }
+            else
+            {
+                object httpFailRequestResultMessage =
+                new { Message = "Unable to obtain brand information." };
+                //Return a bad http response message to the client
+                return BadRequest(httpFailRequestResultMessage);
 
-            return Ok(project);
+
+            }//End of Get(id) Web API method
         }
 
         // PUT: api/Projects/5
@@ -73,7 +128,7 @@ namespace E_Internship_Journal.API
 
                 foundOneProject.ProjectName = projectNewInput.ProjectName;
                 foundOneProject.CompanyID = projectNewInput.CompanyID;
-                foundOneProject.SupervisorId = await _userManager.FindByEmailAsync(projectNewInput.SupervisorEmail);
+                foundOneProject.SupervisorId = (await _userManager.FindByEmailAsync(projectNewInput.SupervisorEmail)).Id;
 
                 _context.Projects.Update(foundOneProject);
                 await _context.SaveChangesAsync();
@@ -94,23 +149,22 @@ namespace E_Internship_Journal.API
             {
                 return BadRequest(ModelState);
             }                //string tqq = "ADMIN@TEST.com";
-                //_userManager.
-                //var userManager = await _userManager.FindByEmailAsync(tqq);
-                //userManager.Id;
-                //var qqq = userManager.Id;
-                //var claims = _userManager.GetClaimsAsync();
-                //_userManager.GetUserId((ClaimsPrincipal)qqq);
-                //_userManager.
-                //var ttt = await _userManager.GetClaimsAsync(userManager);
-                //var qw = _userManager.GetClaimsAsync(userManager);
-                //var user = User;
-                //var iden = (ClaimsIdentity)User;
-                //var claims = _userManager.GetClaimsAsync(userManager);
-                //IEnumerable<Claim> claims = iden.Claims;
-                //var ww = _context.ApplicationUsers.Find("ADMIN@TEST.com");
-                //var ttt = _userManager.GetUserId(userManager);
+                             //_userManager.
+                             //var userManager = await _userManager.FindByEmailAsync(tqq);
+                             //userManager.Id;
+                             //var qqq = userManager.Id;
+                             //var claims = _userManager.GetClaimsAsync();
+                             //_userManager.GetUserId((ClaimsPrincipal)qqq);
+                             //_userManager.
+                             //var ttt = await _userManager.GetClaimsAsync(userManager);
+                             //var qw = _userManager.GetClaimsAsync(userManager);
+                             //var user = User;
+                             //var iden = (ClaimsIdentity)User;
+                             //var claims = _userManager.GetClaimsAsync(userManager);
+                             //IEnumerable<Claim> claims = iden.Claims;
+                             //var ww = _context.ApplicationUsers.Find("ADMIN@TEST.com");
+                             //var ttt = _userManager.GetUserId(userManager);
             string customMessage = "";
-
             try
             {
 
@@ -119,7 +173,7 @@ namespace E_Internship_Journal.API
                 {
                     ProjectName = projectNewInput.ProjectName,
                     CompanyID = projectNewInput.CompanyID,
-                    SupervisorId = await _userManager.FindByEmailAsync(projectNewInput.SupervisorEmail)
+                    SupervisorId = (await _userManager.FindByEmailAsync(projectNewInput.SupervisorEmail)).Id
                 };
                 // newProject.SupervisorId = _userManager.FindByEmailAsync(projectNewInput.SupervisorEmail);
                 //var ttt = _userManager.GetUserId(_userManager.FindByEmailAsync(projectNewInput.SupervisorEmail));
@@ -139,7 +193,7 @@ namespace E_Internship_Journal.API
             };
 
             OkObjectResult httpOkResult =
-new OkObjectResult(successRequestResultMessage);
+            new OkObjectResult(successRequestResultMessage);
             return httpOkResult;
         }
 
