3c8d8a5078b99bbbc92214af6f39f837621a8f1c
Qi Xiang <qixyqix@outlook.com>
2017-11-07 21:18:10 +0800

    Create day record & minor fix to enrollment controller

--- E-Internship Journal/API/Day_RecordController.cs
+++ E-Internship Journal/API/Day_RecordController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using E_Internship_Journal.Data;
 using E_Internship_Journal.Models;
 using Newtonsoft.Json;
+using System.Globalization;
+using Microsoft.AspNetCore.Authorization;
 
 namespace E_Internship_Journal.API
 {
@@ -167,6 +169,7 @@ namespace E_Internship_Journal.API
 
         // POST: api/Day_Record
         [HttpPost("SaveNewDayRecordInformation")]
+        [AllowAnonymous]
         public async Task<IActionResult> SaveNewDayRecordInformation([FromBody] string value)
         {
             if (!ModelState.IsValid)
@@ -177,27 +180,92 @@ namespace E_Internship_Journal.API
             string customMessage = "";
             try
             {
-
                 var day_RecordNewInput = JsonConvert.DeserializeObject<dynamic>(value);
-                Day_Record newDay_Record = new Day_Record
+
+                int internshipRecordId = day_RecordNewInput.InternshipRecordId.Value;
+
+                //Get the student's internship record
+                var internshipRecord = _context.Internship_Records.Include(ir => ir.UserBatch.Batch)
+                    .Include(ir => ir.MonthRecords.Select(mn => mn.DayRecords))
+                    .SingleOrDefault(ir => ir.InternshipRecordId == internshipRecordId);
+
+                DateTime date = DateTime.ParseExact(day_RecordNewInput.Date.Value, "d/M/yyyy", CultureInfo.InvariantCulture);
+
+                Month_Record monthRecord = null;
+
+                if (!(internshipRecord.MonthRecords.Count < 1))
                 {
-                    ArrivalTime = day_RecordNewInput.ArrivalTime.Value,
-                    DepartureTime = day_RecordNewInput.DepartureTime.Value,
-                    CompanyOff = day_RecordNewInput.CompanyOff.Value,
-                    Date = day_RecordNewInput.Date.Value,
-                    MC = day_RecordNewInput.MC.Value,
-                    MonthRecordId = day_RecordNewInput.MonthRecordId.Value,
-                    WeekNo = day_RecordNewInput.WeekNo.Value
+                    //Check if the date already exists
+                    foreach (var month in internshipRecord.MonthRecords)
+                    {
+                        if (month.DayRecords.Count < 36)
+                        {
+                            monthRecord = month;
+                            foreach (var dayRecord in month.DayRecords)
+                            {
+                                if (dayRecord.Date.Equals(date))
+                                {
+                                    return BadRequest();
+                                }
+                            }
+                        }
+                    }
+                }
+                else if(monthRecord == null)
+                {
+                    //Create new month record
+                    monthRecord = new Month_Record
+                    {
+                        InternshipRecord = internshipRecord
+                    };
+                    _context.Month_Records.Add(monthRecord);
+                }
+
+                //TODO VALIDATION FOR ATTENDANCE
+                string[] timeValArray = day_RecordNewInput.ArrivalTime.Value.ToString().Replace(" ","").Split(':');
+                DateTime timeIn = date.AddHours(Int32.Parse(timeValArray[0])).AddMinutes(Int32.Parse(timeValArray[1]));
+                timeValArray = day_RecordNewInput.DepartureTime.Value.ToString().Replace(" ", "").Split(':');
+                DateTime timeOut = date.AddHours(Int32.Parse(timeValArray[0])).AddMinutes(Int32.Parse(timeValArray[1]));
+                var mc = bool.Parse(day_RecordNewInput.MC.Value);
+                var companyOff = bool.Parse(day_RecordNewInput.CompanyOff.Value);
+                var taskRecords = day_RecordNewInput.Tasks;
+
+                //Calculate the weekno
+
+                DateTime startDate = internshipRecord.UserBatch.Batch.StartDate;
+
+                int weekNo = (Int32.Parse((date - startDate).TotalDays.ToString()) /7 ) +1;
+
+                var newDayRecord = new Day_Record
+                {
+                    Date = date,
+                    ArrivalTime = timeIn,
+                    DepartureTime = timeOut,
+                    CompanyOff = companyOff,
+                    MC = mc,
+                    WeekNo = weekNo,
+                    Month = monthRecord
                 };
 
-                _context.Day_Records.Add(newDay_Record);
+                List<Task_Record> tasks = new List<Task_Record>();
+                for (int i = 0; i < taskRecords.Count; i++) {
+                    var taskRecord = new Task_Record
+                    {
+                        Description = taskRecords[i].Value
+                    };
+
+                    tasks.Add(taskRecord);
+                }
+
+                newDayRecord.Tasks = tasks;
+
+                _context.Day_Records.Add(newDayRecord);
                 await _context.SaveChangesAsync();
 
             }
             catch (Exception exceptionObject)
             {
-                customMessage = "Unable to save to database";
-                //return 
+                customMessage = "Unable to save to database"; 
             }
             var successRequestResultMessage = new
             {
