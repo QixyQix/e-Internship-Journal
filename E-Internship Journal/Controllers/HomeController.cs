4d8ade7ca29975cd86c3a6a6fd7055115df35e6b
Chia Wei <tanchiawei.15@ichat.sp.edu.sg>
2017-11-08 22:05:41 +0800

    Re-configure Home Controller to handle Admin Page

--- E-Internship Journal/Controllers/HomeController.cs
+++ E-Internship Journal/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace E_Internship_Journal.Controllers
                 return View("~/Views/Home/Admin/Index.cshtml");
             }
         }
+        
         public IActionResult Grading()
         {
             var test = User.IsInRole("SUPERVISOR");
@@ -223,5 +224,60 @@ namespace E_Internship_Journal.Controllers
         {
             return View();
         }
+
+
+        //Admin
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateBatch()
+        {
+            return View("~/Views/Home/Admin/Add_Batch.cshtml");
+        }
+        
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditBatch()
+        {
+            return View("~/Views/Home/Admin/Edit_Batch.cshtml");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult ViewBatch()
+        {
+            return View("~/Views/Home/Admin/Batch.cshtml");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult ViewCompany()
+        {
+            return View("~/Views/Home/Admin/Company.cshtml");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateComapny()
+        {
+            return View("~/Views/Home/Admin/Add_Company.cshtml");
+        }
+        //public IActionResult EditCompany()
+        //{
+        //    return View("~/Views/Home/LO/touchpoint.cshtml");
+        //}
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult ViewUser()
+        {
+            return View("~/Views/Home/Admin/User.cshtml");
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateUser()
+        {
+            return View("~/Views/Home/Admin/Add_User.cshtml");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditUser()
+        {
+            return View("~/Views/Home/Admin/Edit_User.cshtml");
+        }
+
+
     }
 }
